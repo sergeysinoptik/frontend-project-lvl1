15f78c2d61e42d24a8ac7b2c340479b61c345aba
Sergey Stoyko <sergeysinoptik@gmail.com>
2021-09-17 17:24:11 +0300

    step 7 - add brain-gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import engine from '../src/index.js';
+import * as gcd from '../src/games/gcd.js';
+
+engine(gcd);
---
+++ src/games/gcd.js
@@ -0,0 +1,32 @@
+export const rules = 'Find the greatest common divisor of given numbers.';
+
+const getRandomNumber = () => Math.floor(Math.random() * 999);
+
+const gcd = (firstNum, secondNum) => {
+  if (firstNum % secondNum === 0) {
+    return secondNum;
+  }
+  let divisor = secondNum;
+  let i = firstNum % secondNum;
+  let result = 1;
+  while (divisor % i > 0) {
+    result = divisor % i;
+    divisor = i;
+    i = result;
+  }
+  return result;
+};
+
+export const correctAnswer = (str) => {
+  const answerArray = str.split(' ');
+  answerArray.sort((a, b) => b - a);
+  return String(gcd(answerArray[0], answerArray[1]));
+};
+
+const getTwoRandomNumbers = () => {
+  const firstNumber = getRandomNumber();
+  const secondNumber = getRandomNumber();
+  return `${firstNumber} ${secondNumber}`;
+};
+
+export const gameQuestion = () => getTwoRandomNumbers();
